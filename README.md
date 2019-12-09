de351bcdecd85094ef741dfdf795b6d6aed76b58
caridadrivera <ccrive10@gmail.com>
2019-12-09 11:47:26 -0500

    linkedlist getFirst

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Companion repo to a course on Udemy.com
 # tests_algos_DS
+# tests_algos_DS
--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,31 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        
+        this.data = data
+        //if someone sends in a datatype, but does not pass in a new one to link up, it should be null
+        this.next = next 
+    }
+}
 
-class LinkedList {}
+
+class LinkedList {
+
+    constructor() {
+        this.head  = null
+    }
+
+    insertFirst(data){
+        const node = new Node(data, this.head)
+        this.head = node;
+    }
+
+   
+
+
+
+}
 
 module.exports = { Node, LinkedList };
